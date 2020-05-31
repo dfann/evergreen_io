b159bdb74673c7028002dde179cc690d954c3917
MacBook <macbook@MacBooks-MacBook-Pro.local>
2020-05-31 10:06:12 -0400

    Creation of Landing Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+    <Jumbotron className="Jumbo">
+      <Container fluid className="App">
+        <Row>                    
+            <Col xs={{ span:2 }}><img src={logo}  className="App-logo" alt="logo" /></Col>
+            <Col xs={{offset: 2, span: 8}} className="Logo-text">
+            <p className="Logo-max-text">
+              Evergreen.io
+            </p>          
+            <p className="Logo-min-text">Remember Everthing</p>
+            <ButtonGroup className="Login-Buttons">
+            <Button className="btn btn-success">Sign Up</Button>
+            <Button className="btn btn-secondary">Log In</Button>
+            </ButtonGroup>
+            </Col>        
+        </Row> 
+      </Container>
+    </Jumbotron>
+    </header>
   );
 }
 
