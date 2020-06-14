1209544b49c412c984cce5769c34c2e096fab649
MacBook <macbook@MacBooks-MBP.fios-router.home>
2020-06-14 19:06:09 -0400

    Adding links to view all questions and add new question

--- src/frontend/components/EvergreenNavbar.js
+++ src/frontend/components/EvergreenNavbar.js
@@ -4,11 +4,18 @@ import Navbar from 'react-bootstrap/Navbar';
 import logo from '../logo.svg';
 import SessionContext from '../context/session-context';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 const EvergreenNavbar = () => {
     const session = useContext(SessionContext);
     const logOut = session.session.username ? (
         <Nav className="ml-auto">
+            <Link to="/questions">
+                <Button variant="link">View Question</Button>
+            </Link>
+            <Link to="/add/question">
+                <Button variant="link">Add Question</Button>
+            </Link>
             <Button onClick={session.logout} variant="link">
                 Logout
             </Button>
