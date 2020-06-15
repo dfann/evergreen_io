4729f7be2fc6184d06e1ec2c8f2e73d98edcb8c7
MacBook <macbook@MacBooks-MBP.fios-router.home>
2020-06-14 23:01:57 -0400

    Using background imagefor site

--- src/frontend/util/question.js
+++ src/frontend/util/question.js
@@ -1,4 +1,4 @@
-export const new = async (question) => {
+export const newQuestion = async (question) => {
     const response = await fetch('http://localhost:3000/api/questions', {
         method: 'POST',
         mode: 'cors', // no-cors,
